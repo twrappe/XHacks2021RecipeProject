12394539fe10507d7149bb0fa2ca1236de7c21d6
Thomas Wrappe <twrappe@gmail.com>
2021-08-08 02:41:33 -0700

    Backend fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,17 @@
 from model import *
 from flask import jsonify
 login = LoginManager()
-
- 
+from sqlalchemy import exc
+from flask_cors import CORS
  
 @login.user_loader
 def load_user(id):
     return UserModel.query.get(int(id))
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///username_pwd.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 #db = SQLAlchemy(app)
 app.secret_key= "asdfghjklwertyuifkyou"
 db.init_app(app)
@@ -20,7 +21,7 @@ login.login_view = 'login'
 @app.before_first_request
 def create_all():
     db.create_all()
-     
+
 @app.route('/')
 def home():
     return '''
@@ -66,3 +67,31 @@ def logout():
     logout_user()
     return redirect('/myrecipes')
 
+@app.route('/search',methods = ['POST', 'GET'])
+def searching():
+    if request.method == "GET":
+        recipes = RecipeModel.query.all()
+        results = []
+        for recipe in recipes:
+            result = {}
+            result["image"] = recipe.image
+            result["name"] = recipe.name
+            results.append(result)
+        return jsonify(results)
+@app.route('/add', methods = ['GET', 'POST'])
+def add_recipe():
+    if request.method == "GET":
+        name = "Cookies"
+        image = "../img/4_pastry.jpg"
+        recipe = RecipeModel(3, 1, 1, name, image)
+        try:
+            db.session.add(recipe)
+            db.session.commit()
+        except exc.IntegrityError:
+            db.session.rollback()
+        return jsonify(success=True)
+@app.route('/clear', methods=['GET'])
+def clear_db():
+    db.create_all()
+    return jsonify(success=True)
+app.run(debug=True)
